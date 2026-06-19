3bc82c731f26d1fe84ee7f89cf05489e706fcf61
USER922-44 <user@USER922-44.local>
2026-06-19 11:13:03 +0700

    คำนวณ

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+      <h1>งานที่2 ณัฏฐณิชา เลขที่44 BIT.2/5</h1>
+
+        <!-- คะแนนช่อง 1 = 10 -->
+        <!-- คะแนนช่อง 2 = 20 -->
+        <!-- คะแนนรวม = คะแนนช่องที่ 1 + คะแนนช่องที่ 2 -->
+        <!-- output. = input1. + input2. -->
+        <!-- 30.  =10.       + 20. -->
+
+        <!-- ข้อมูลจะส่งไปที่ไหน.        ส่งแบบไหน -->
+         <!-- <form action="result.php" method="post">
+            
+            <label for="student_name">นักศึกษา:</label>
+
+            <input type="text" name="student_name"> <br>
+
+            <label for="score1">คะแนนช่อง 1:</label>
+            <input type="number" name="num1" > <br>
+
+            <label for="score1">คะแนนช่อง 2:</label>
+            <input type="number" name="num2"> <br>
+
+            <button type="submit">คำนวณ</button>
+        </form> -->
+        <form action="result.php" method="post">
+
+        <label></label>
+
+        </form>
+           
+</body>
+</html>
---
+++ result.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <!-- <?php
+        $student_name = $_POST['student_name'];
+        $num1 = $_POST['num1'];
+        $num2 = $_POST['num2'];
+        
+        echo "ชื่อนักศึกษา : " . $student_name . "<br>";
+        echo "คะแนนช่อง 1 : " . $num1 . "<br>";
+        echo "คะแนนช่อง 2 : " . $num2 . "<br>";
+        
+        $total = $num1 + $num2; 
+        echo"คะแนนรวม :" . $total . "<br>";
+    ?> -->
+
+    <!-- <a href="index.php">กลับไปหน้าแรก</a> -->
+</body>
+</html>
